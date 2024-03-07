7f2c5d8a3fbea60d4a9089532425c446873bbfc5
Shraddha Salunke <91833741+Shraddhasalunke@users.noreply.github.com>
2024-03-07 13:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiple.text",{
+    string: ["FrontEnd Developer","Backend Developer", "Graphic Designer"],
+    typeSpeed:100,
+    backspeed: 1000,
+    backDelay: 1000,
+    loop:true
+})
